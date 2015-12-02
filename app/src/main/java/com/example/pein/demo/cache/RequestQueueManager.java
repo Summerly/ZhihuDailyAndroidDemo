70f8847780406d7db90b54af86f70e8662fb96b7
xiyuexin <Pein@XiYuexin-MacBook-Pro.local>
2015-12-02 14:08:04 +0800

    更换默认Logo

--- app/src/main/java/com/example/pein/demo/cache/RequestQueueManager.java
+++ app/src/main/java/com/example/pein/demo/cache/RequestQueueManager.java
@@ -1,7 +1,26 @@
 package com.example.pein.demo.cache;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.example.pein.demo.DemoApplication;
+
 /**
  * Created by Pein on 15/12/2.
  */
 public class RequestQueueManager {
+    public static RequestQueue sRequestQueue = Volley.newRequestQueue(DemoApplication.getInstance());
+
+    public static void addRequest(Request<?> request, Object object) {
+        if (object != null) {
+            request.setTag(object);
+        }
+
+        sRequestQueue.add(request);
+    }
+
+    public static void cancelAll(Object tag) {
+        sRequestQueue.cancelAll(tag);
+    }
 }
+
