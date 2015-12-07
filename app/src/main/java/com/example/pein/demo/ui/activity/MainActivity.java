abbea70e58968d0c7ee1371afd7023ed61967f15
Summerly.Xi <Summerly.Xi@gmail.com>
2015-12-07 17:01:29 +0800

    add CircularFloatingActionMenu

--- app/src/main/java/com/example/pein/demo/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/pein/demo/ui/activity/MainActivity.java
@@ -2,26 +2,18 @@ package com.example.pein.demo.ui.activity;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.app.ProgressDialog;
-import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-import com.example.pein.demo.Constants;
 import com.example.pein.demo.R;
-import com.example.pein.demo.cache.RequestQueueManager;
 import com.example.pein.demo.database.DemoDatabase;
 import com.example.pein.demo.ui.fragment.LatestFragment;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
 import com.orhanobut.logger.Logger;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "XiYuexin";
@@ -43,6 +35,22 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragmentContainer, fragment)
                     .commit();
         }
+
+        ImageView icon = new ImageView(this);
+        Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_tag_faces_black);
+        icon.setImageDrawable(drawable);
+
+        FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+                                                .setContentView(icon)
+                                                .build();
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Logger.e("actionButton click");
+            }
+        });
+
     }
 
     @Override
